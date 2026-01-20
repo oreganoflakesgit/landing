560842d9ac77beb8c012633cef54244ab6216cc9
Paweł Starczewski <pvz@WS-JQ14NTDRH9.local>
2026-01-20 21:05:47 +0100

    Add GitHub Pages deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+const repo = "landing";
+
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
   images: {
     unoptimized: true
   },
