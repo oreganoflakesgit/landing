934e4ac72054c36be5b28a748a29602d3c5929e3
oreganoflakes <hi@oreganoflakes.com>
2026-03-03 19:11:14 +0100

    Add selected staysaasy quotes

--- data/quotes.js
+++ data/quotes.js
@@ -55,5 +55,37 @@ export const quotes = [
     authorUrl: "https://x.com/halvarflake/status/2027691153106079868",
     primaryHashtag: "life",
     secondaryHashtags: []
+  }),
+  createQuote({
+    id: "staysaasy-managed-teams-emergencies",
+    text: "It's remarkable how well managed teams basically never have emergencies and badly managed teams have emergencies constantly",
+    author: "staysaasy",
+    authorUrl: "https://x.com/staysaasy/status/2027767930336010698",
+    primaryHashtag: "management",
+    secondaryHashtags: []
+  }),
+  createQuote({
+    id: "staysaasy-world-runs-on-effort",
+    text: "The worst curse that you can inflict on someone is the curse of believing that the world runs on magic. You talk and the deal closes. You write user stories and the product gets launched. No. Not at all. The opposite. The world runs on effort, organization, follow-up. Only.",
+    author: "staysaasy",
+    authorUrl: "https://x.com/staysaasy/status/2027132385435820097",
+    primaryHashtag: "management",
+    secondaryHashtags: ["life"]
+  }),
+  createQuote({
+    id: "staysaasy-disagreeing-culture",
+    text: "Having a culture where it’s see as immature to disagree is the worst kind of culture there is.",
+    author: "staysaasy",
+    authorUrl: "https://x.com/staysaasy/status/2026346288631923072",
+    primaryHashtag: "management",
+    secondaryHashtags: ["life"]
+  }),
+  createQuote({
+    id: "staysaasy-tech-debt-skill-issue",
+    text: "Being unable to describe, prove, and easily display the value of doing tech debt is a skill issue.",
+    author: "staysaasy",
+    authorUrl: "https://x.com/staysaasy/status/2026723653757325693",
+    primaryHashtag: "engineering",
+    secondaryHashtags: []
   })
 ];
